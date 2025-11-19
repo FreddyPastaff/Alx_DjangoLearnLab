f545829197e16a6080d95325990939a84e154978
Freddy Pastaff <amoakof92@gmail.com>
2025-11-19 06:06:26 +0000

    Move user profile creation logic to signals.py for better organization

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
@@ -19,11 +18,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-"""CreatingNewUser"""
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
