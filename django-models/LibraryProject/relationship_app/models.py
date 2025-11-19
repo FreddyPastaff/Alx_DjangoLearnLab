f67c539dcc03c2756b291039d94a2ae324b6be5e
Freddy Pastaff <amoakof92@gmail.com>
2025-11-19 05:08:18 +0000

    Add admin role check and view for user management

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import UserProfile
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 
 """User Model"""
@@ -25,6 +25,13 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+"""Admin"""
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
