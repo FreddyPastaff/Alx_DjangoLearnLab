225d1ba2e95b50b6348842f25028aea16dbb61fc
Freddy Pastaff <amoakof92@gmail.com>
2025-11-19 03:48:15 +0000

    Refactor URL patterns to use standard authentication views and streamline imports

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView
-from .views import register_view, CustomLoginView, CustomLogoutView
-from . import views
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register_view, name='register'),
-]
-
-urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
