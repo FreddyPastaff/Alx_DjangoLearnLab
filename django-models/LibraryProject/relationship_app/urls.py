61765cfd5488921df2bb5b13af7ca0162ebd60da
Freddy Pastaff <amoakof92@gmail.com>
2025-11-19 04:52:56 +0000

    Reorganize URL patterns for improved readability and consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView
 from .views import register
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 from . import views
 
 urlpatterns = [
@@ -10,10 +12,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view')
+    path('member-view/', member_view, name='member_view'),
+
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail') 
 ]
