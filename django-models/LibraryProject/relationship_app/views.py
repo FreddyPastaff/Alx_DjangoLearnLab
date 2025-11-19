ad7de293631edc4ba9c29e351e883952573e7aa9
Freddy Pastaff <amoakof92@gmail.com>
2025-11-19 02:49:20 +0000

    Refactor views to correct template paths and optimize book queries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-from .models import Book
 from django.views.generic.detail import DetailView
-from .models import Library
+from .models import Book, Library
 
-
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # Required for the check
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Required template path
 
+# Class-based view to show library details and its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.book_set.select_related('author').all()
+        context['books'] = self.object.book_set.all()
         return context
         
 
