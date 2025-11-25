e7fd02af0071190ea656c84f48bd00c9891db60c
Freddy Pastaff <amoakof92@gmail.com>
2025-11-25 15:25:45 +0000

    Add bookshelf app and enhance admin interface for Book and CustomUser models

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  
-    search_fields = ('title', 'author')  
-    list_filter = ('publication_year',)  
 
-admin.site.register(Book, BookAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("publication_year", "author")
+    search_fields = ("title", "author")
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo", "phone_number", "bio", "membership_type", "role")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+   # add_fieldsets control the layout of the create form
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active", "date_of_birth", "profile_photo", "phone_number", "bio", "membership_type", "role"),
+        }),
+    )             
+                
+    
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.site_header = "Bookshelf Admin"
+admin.site.site_title = "Bookshelf Admin Portal"
