3085e14f93d597d6362e9f7dddd3d86cc1630ed2
Freddy Pastaff <amoakof92@gmail.com>
2025-11-25 15:31:49 +0000

    Add custom user model configuration and update logout redirect URL

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -125,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+AuUTH_USER_MODEL = 'bookshelf.CustomUser'  # Custom user model
