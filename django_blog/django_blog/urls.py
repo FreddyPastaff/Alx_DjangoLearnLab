d43a83decc218fff4fbc76f29358a0ce205a3ab5
Freddy Pastaff <amoakof92@gmail.com>
2025-12-15 15:51:29 +0000

    Refactor imports in urls.py for clarity and add user authentication views.

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import UserLoginView, UserLogoutView, register, profile
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
