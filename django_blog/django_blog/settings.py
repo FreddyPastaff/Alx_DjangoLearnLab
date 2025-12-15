2ae625d4b8a98c871bbe10c4d337622067700d64
Freddy Pastaff <amoakof92@gmail.com>
2025-12-15 16:17:01 +0000

    Update login and logout redirect URLs to point to blog app views

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -130,6 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = "blog:profile"
+LOGOUT_REDIRECT_URL = "blog:login"
+LOGIN_URL = "blog:login"
+LOGIN_REDIRECT_URL = 'blog:post_list'
+LOGOUT_REDIRECT_URL = 'blog:post_list'
 
 # Media (for profile pictures)
 MEDIA_URL = '/media/'
